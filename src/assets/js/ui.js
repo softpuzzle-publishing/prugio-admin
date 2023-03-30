6eda226498f6363121e8a87ccd7bd66851ff472d
kimhana <hnone99@gmail.com>
2023-03-30 19:04:23 +0900

    최초커밋

---
+++ src/assets/js/ui.js
@@ -0,0 +1,76 @@
+let Common = {
+  init: function () {
+    this.common();
+  },
+  common: function () {
+    //datepicker
+    /* $('[data-picker="date"]').each(function () {
+      $(this).datepicker({
+        monthNames: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+        monthNamesShort: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"],
+        dayNamesMin: ["일", "월", "화", "수", "목", "금", "토"],
+        showMonthAfterYear: true,
+        showOtherMonths: true,
+        changeMonth: true,
+        changeYear: true,
+        dateFormat: "yy.mm.dd",
+      });
+    }); */
+    var datePickerInputs = [];
+    $('[data-picker="date"]').each(function (i) {
+      // eslint-disable-next-line no-undef
+      datePickerInputs[i] = new tempusDominus.TempusDominus($(this)[0], {
+        display: {
+          theme: "dark",
+          viewMode: "calendar",
+          components: {
+            clock: false,
+          },
+        },
+      });
+      // eslint-disable-next-line no-undef
+      datePickerInputs[i].dates.formatInput = (date) => moment(date).format("YYYY.MM.DD");
+    });
+
+    //datetimepicker
+    var dateTimePickerInputs = [];
+    $('[data-picker="datetime"]').each(function (i) {
+      // eslint-disable-next-line no-undef
+      dateTimePickerInputs[i] = new tempusDominus.TempusDominus($(this)[0], {
+        display: {
+          sideBySide: true,
+          theme: "dark",
+        },
+      });
+      // eslint-disable-next-line no-undef
+      dateTimePickerInputs[i].dates.formatInput = (date) => moment(date).format("YYYY.MM.DD HH:mm");
+    });
+
+    //timepicker
+    $('[data-picker="color"]').each(function (i) {
+      $(this).spectrum({
+        type: "component",
+        showPalette: true,
+        togglePaletteOnly: true,
+      });
+    });
+  },
+};
+
+let Aside = {
+  init: function () {
+    this.toggle();
+  },
+  toggle: function () {
+    $(".sidebar-nav > li > a").on("click", function (e) {
+      let $target = $(this).parent();
+      if ($target.hasClass("has-treeview")) {
+        e.preventDefault();
+        $target.toggleClass("active");
+      }
+    });
+  },
+};
+
+Common.init();
+Aside.init();
